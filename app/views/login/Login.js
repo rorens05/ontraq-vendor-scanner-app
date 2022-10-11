913c865e79ff8daee01143ef08ba72037d07b079
Adonis Sarmiento <sarmiento.vam@gmail.com>
2022-10-11 15:41:30 +0800

    Code Screen

--- app/views/login/Login.js
+++ app/views/login/Login.js
@@ -20,58 +20,12 @@ export default function Login() {
   };
   return (
     <View style={[styles.flex_1, styles.bg_white, styles.align_center_justify_space_evenly]}>
-      <View>
-        <Image source={logo} style={styles.variant_200_200} />
-      </View>
       <View style={[styles.px_5, styles.align_items_center]}>
-        <Text style={[styles.h1, styles.text_blue]}>Hello!</Text>
-        <Text style={{color: '#9A9A9A', fontSize: 16}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</Text>
+        <Text style={[styles.h5, styles.text_gray]}>Please enter this code to your device</Text>
+        <Text style={[styles.text_gray, {fontSize: 50, fontWeight: 'bold'}]}>QWE123</Text>
       </View>
       <View style={{alignItems: 'center'}}>
-        <Button label={'Sign in'} onPress={() => navigation.navigate('LoginScreen')} />
-        <View style={{alignItems: 'center', flexDirection: 'row', paddingVertical: 10}}>
-          <View style={styles.hr_line} />
-          <Text style={[styles.text_gray, styles.px_1]}>OR</Text>
-          <View style={styles.hr_line} />
-        </View>
-        <View
-        style={{
-          flex: 1,
-          backgroundColor: '#EE9337',
-          marginVertical: 50,
-          marginHorizontal: 20,
-        }}>
-        <QRCodeScanner
-          cameraStyle={{width: '100%', height: '100%'}}
-          cameraContainerStyle={{
-            overflow: 'hidden',
-            backgroundColor: 'transparent',
-          }}
-          cameraType="back"
-          onRead={handleScan}
-          // flashMode={RNCamera.Constants.FlashMode.torch}
-        />
-        <View
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            justifyContent: 'center',
-            alignItems: 'center',
-            zIndex: 999,
-          }}>
-          <Text style={{color: '#fff', fontWeight: 'bold'}}>
-            Please put the QR Code within the box
-          </Text>
-          <Image
-            source={require('../../assets/qrscanner.png')}
-            style={{width: width / 1.5, height: width / 1.5, tintColor: '#fff'}}
-            resizeMode="contain"
-          />
-        </View>
-      </View>
+        <Button label={'Reload'} onPress={() => navigation.navigate('LoginScreen')} />
       </View>
     </View>
   )
--- app/views/splashscreen/SplashScreen.js
+++ app/views/splashscreen/SplashScreen.js
@@ -7,7 +7,7 @@ const navigation = React.useContext(NavigationContext)
 
   React.useEffect(() => {
     const timer = setTimeout(() => {
-      navigation.replace('QrScanner')
+      navigation.replace('Login')
     }, 3000);
     return () => {
       clearTimeout(timer)
