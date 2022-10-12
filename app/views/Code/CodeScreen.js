e3d623b55eb47f02f243c8f07f3bd57c5a170226
Adonis Sarmiento <sarmiento.vam@gmail.com>
2022-10-12 15:05:57 +0800

    Remove unused components

--- app/views/Code/CodeScreen.js
+++ app/views/Code/CodeScreen.js
@@ -1,6 +1,6 @@
 import React, {useContext,useState,useEffect} from 'react'
 import {View, Text} from 'react-native'
-import Button from './components/Button'
+import Button from '../../components/Button'
 import { NavigationContext } from '@react-navigation/native';
 import styles from '../../styles';
 import AsyncStorage from '@react-native-async-storage/async-storage';
