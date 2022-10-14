4691f614a8588ccce7f5608af06b0722927f021e
Adonis Sarmiento <sarmiento.vam@gmail.com>
2022-10-14 23:14:56 +0800

    Fix Settings

--- app/views/Settings/Settings.js
+++ app/views/Settings/Settings.js
@@ -7,7 +7,7 @@ export default function Settings() {
   const navigation = useContext(NavigationContext);
   return (
     <View style={{ flex: 1, backgroundColor: '#fff', justifyContent: 'center', alignItems: 'center' }}>
-      <Text>{'Under Development'}</Text>
+      <Text>{'Under Development.'}</Text>
     </View>
   )
 }
