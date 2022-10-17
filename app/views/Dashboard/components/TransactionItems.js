bdc0999f2aa6610f8a19cc1ae51191be9b4b9276
Adonis Sarmiento <sarmiento.vam@gmail.com>
2022-10-17 20:15:34 +0800

    Integrate Transactions

--- app/views/Dashboard/components/TransactionItems.js
+++ app/views/Dashboard/components/TransactionItems.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import styles from '../../../styles';
+import moment from 'moment';
+import { currencyFormat } from '../../../utils/MoneyConverter';
 
-export default function TransactionItem({item}) {
+export default function TransactionItem({item, key}) {
+  
   return (
-    <View style={[styles.py_3, styles.px_2]}>
+    <View key={key} style={[styles.py_3, styles.px_2]}>
     <View style={[styles.flex_row, styles.justify_content_space_between]}>
       <Text style={[styles.text_gray]}>{'Received money from'}</Text>
-      <Text style={[styles.text_gray]}>{'2 Oct. 2022'}</Text>
+      <Text style={[styles.text_gray]}>{moment(item?.transaction_datetime).format('DD/MM/YYYY')}</Text>
     </View>
     <View style={[styles.flex_row, styles.justify_content_space_between]}>
       <Text style={[styles.h2, styles.text_blue]}>{'Juan Dela Cruz'}</Text>
-      <Text style={[styles.h2, styles.text_green]}>{'+450.00'}</Text>
+      <Text style={[styles.h2, styles.text_green]}>{`+ ${currencyFormat(parseFloat(item?.amount))}`}</Text>
     </View>
   </View>
   )
