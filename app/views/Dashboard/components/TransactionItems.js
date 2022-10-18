e05a4279b14bd80700f2fcf6c74f23cce32a2925
Adonis Sarmiento <sarmiento.vam@gmail.com>
2022-10-18 11:15:26 +0800

    Integrate Transactions

--- app/views/Dashboard/components/TransactionItems.js
+++ app/views/Dashboard/components/TransactionItems.js
@@ -10,10 +10,10 @@ export default function TransactionItem({item, key}) {
     <View key={key} style={[styles.py_3, styles.px_2]}>
     <View style={[styles.flex_row, styles.justify_content_space_between]}>
       <Text style={[styles.text_gray]}>{'Received money from'}</Text>
-      <Text style={[styles.text_gray]}>{moment(item?.transaction_datetime).format('DD/MM/YYYY')}</Text>
+      <Text style={[styles.text_gray]}>{moment(item?.transaction_datetime).format('MM/DD/YYYY')}</Text>
     </View>
     <View style={[styles.flex_row, styles.justify_content_space_between]}>
-      <Text style={[styles.h2, styles.text_blue]}>{'Juan Dela Cruz'}</Text>
+      <Text style={[styles.h2, styles.text_blue]}>{item?.name}</Text>
       <Text style={[styles.h2, styles.text_green]}>{`+ ${currencyFormat(parseFloat(item?.amount))}`}</Text>
     </View>
   </View>
